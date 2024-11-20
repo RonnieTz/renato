7c68596196b3bb0a2e30a9525a4ddc6e827ac97f
ronis <ronis.tzol@gmail.com>
2024-11-20 21:35:41 +0000

    push image

--- src/feutures/home/HomePage.tsx
+++ src/feutures/home/HomePage.tsx
@@ -1,6 +1,11 @@
 import './home_styles.css';
+import image from '../../assets/20230705_144111.jpg';
 
 const HomePage = () => {
-  return <main className="home">Home Page</main>;
+  return (
+    <main className="home">
+      <img style={{ width: '100%' }} src={image} alt="image" />
+    </main>
+  );
 };
 export default HomePage;
